5dab167c88bd4028e98d18d789b08ca9035cacbb
stanusiucristina <cristina.stanusiu@coreai.ai>
2020-11-22 14:48:10 +0200

    design AddEventForm

--- Components/AddEventForm.jsx
+++ Components/AddEventForm.jsx
@@ -1,120 +1,158 @@
-import React from 'react';
-import {StyleSheet,
-        ScrollView,
-        View,
-        TextInput,
-        TouchableOpacity,
-        Text,
-        Button} from 'react-native';
-import {Formik} from 'formik';
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    ScrollView,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Text
+} from 'react-native';
+import { Formik } from 'formik';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import moment from 'moment';
+import { onChange } from 'react-native-reanimated';
 
-export default function AddEventForm({addEvent}){
+export default function AddEventForm({ addEvent }) {
+    const [isVisible, setPickerOpen] = useState(false);
+    const [chosenDate, setChosenDate] = useState('');
 
-    return(
+    return (
         <ScrollView>
-        <Text style={styles.motto}>Let your friends know your plans</Text>
-        <View style={StyleSheet.container}>
-            <Formik
-                initialValues={{title:'',place:'',date:'',time:'',maxPers:0}}
-                onSubmit={(values,actions) => { 
-                    //add functionality here 
-                    actions.resetForm();
-                    addEvent(values);
-                    console.log(values);
-                }}
+            <Text style={styles.motto}>Let your friends know your plans</Text>
+            <View style={StyleSheet.container}>
+                <Formik
+                    initialValues={{ title: '', place: '', datetime: '', maxPers: 0 }}
+                    onSubmit={(values, actions) => {
+                        //add functionality here 
+                        // actions.resetForm();
+                        values.datetime = chosenDate;
+                        addEvent(values);
+                        console.log(values);
+                    }}
                 >
-                {props => (
-                    <View style={styles.formik}>
-                        <TextInput 
-                        style={styles.input}
-                        placeholder='Event title'
-                        onChangeText={props.handleChange('title')}
-                        values={props.values.title}
-                        />
+                    {props => (
+                        <View style={styles.formik}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Event title'
+                                onChangeText={props.handleChange('title')}
+                                values={props.values.title}
+                            />
 
-                        <TextInput 
-                        style={styles.input}
-                        placeholder='Place'
-                        onChangeText={props.handleChange('place')}
-                        values={props.values.place}
-                        />
-                        
-                        <TextInput 
-                        style={styles.input}
-                        placeholder='Date'
-                        onChangeText={props.handleChange('date')}
-                        values={props.values.date}
-                        />
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Place'
+                                onChangeText={props.handleChange('place')}
+                                values={props.values.place}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Maximum Persons'
+                                onChangeText={props.handleChange('maxPers')}
+                                values={props.values.maxPers}
+                                keyboardType='number-pad'
+                            />
 
-                        <TextInput 
-                        style={styles.input}
-                        placeholder='Time'
-                        onChangeText={props.handleChange('time')}
-                        values={props.values.time}
-                        />
+                            <TouchableOpacity
+                                style={styles.datetimeButton}
+                                onPress={() => setPickerOpen(true)} >
+                                <Text style={styles.datetime}>Date & Time</Text>
+                            </TouchableOpacity>
 
-                        
-                        <TextInput 
-                        style={styles.input}
-                        placeholder='Maximum Persons'
-                        onChangeText={props.handleChange('maxPers')}
-                        values={props.values.maxPers}
-                        />
-                        
-                         <TouchableOpacity
-                            style={styles.submitButton}
-                            onPress={props.handleSubmit} >
-                            <Text style={styles.submitText}>Submit</Text>
-                        </TouchableOpacity>
-                    </View>
-                )}
-            </Formik>
-        </View>
+                            <DateTimePicker
+                                isVisible={isVisible}
+                                onConfirm={(datetime) => {
+                                    setChosenDate(moment(datetime).format("HH:mm, Do MMM"));
+                                    setPickerOpen(false);
+                                }}
+                                onCancel={() => {
+                                    setPickerOpen(false);
+                                }}
+                                mode={'datetime'}
+                                is24Hour={false}
+                                onChangeText={() => props.handleChange('datetime')}
+                                values={props.values.datetime}
+                            />
+                            <Text
+                                style={styles.chosenDate}
+                                placeholder='Date and Time'
+                            >{chosenDate}</Text>
+
+                            <TouchableOpacity
+                                style={styles.submitButton}
+                                onPress={props.handleSubmit}                                >
+                                <Text style={styles.submitText}>Submit</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </Formik>
+            </View>
         </ScrollView>
 
     );
-        }
+}
 
 const styles = StyleSheet.create({
-    formik:{
-        marginTop:8,
-        padding:20,
-        
+    formik: {
+        marginTop: 8,
+        padding: 20,
+
     },
-    input:{
-            borderWidth:2,
-            borderColor:'#D9C6BF',
-            padding:10,
-            fontSize:18,
-            borderRadius:15,
-            marginTop:15
+    input: {
+        borderWidth: 2,
+        borderColor: '#D9C6BF',
+        padding: 10,
+        fontSize: 18,
+        borderRadius: 15,
+        marginTop: 15
     },
-    submitButton:{
-            marginTop:20,
-            marginLeft:'20%',
-            width: 200,
-            height: 70,
-            color:'#8C625E',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 10,
-            borderRadius: 100,
-            backgroundColor: '#F2E0D5',      
-            opacity:1  
+    submitButton: {
+        marginTop: 20,
+        marginLeft: '20%',
+        width: 200,
+        height: 70,
+        color: '#8C625E',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        borderRadius: 100,
+        backgroundColor: '#F2E0D5',
+        opacity: 1
     },
-    submitText:{
-        fontSize:30,
-        color:'#8C625E'
+    submitText: {
+        fontSize: 30,
+        color: '#8C625E'
     },
-    motto:{
-        marginTop:140,
-        fontSize:30,
-        alignSelf:'center',
-        color:'#8C625E',
-        marginHorizontal:60,
+    motto: {
+        marginTop: 140,
+        fontSize: 30,
+        alignSelf: 'center',
+        color: '#8C625E',
+        marginHorizontal: 60,
+    },
+    datetimeButton: {
+        marginTop: 20,
+        width: '100%',
+        height: 50,
+        color: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        borderRadius: 15,
+        backgroundColor: '#8C625E',
+        opacity: 1
+    },
+    datetime: {
+        fontSize: 20,
+        color: '#fff'
+    },
+    chosenDate: {
+        fontSize: 20,
+        color: '#CF1C1C',
+        alignSelf: 'center'
     }
 });
-        
+
 
         // <View style={{
         //     flexDirection:'row',
@@ -131,3 +169,20 @@ const styles = StyleSheet.create({
         //     <TextInput style={{paddingHorizontal:10}}></TextInput>
 
         // </View>
+
+
+
+        // <TextInput 
+        // style={styles.input}
+        // placeholder='Date'
+        // onChangeText={props.handleChange('date')}
+        // values={props.values.date}
+        // />
+
+        // <TextInput 
+        // style={styles.input}
+        // placeholder='Time'
+        // onChangeText={props.handleChange('time')}
+        // values={props.values.time}
+        // />
+
--- Components/EventsScreen.jsx
+++ Components/EventsScreen.jsx
@@ -1,30 +1,32 @@
-import React, {useState} from 'react';
-import {StyleSheet,
-        Text,
-        View,
-        ScrollView,
-        Image,
-        TouchableOpacity,
-        Modal} from 'react-native';
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    Image,
+    TouchableOpacity,
+    Modal
+} from 'react-native';
 import Card from "./shared/Card";
-import { AntDesign } from '@expo/vector-icons'; 
-import { Entypo } from '@expo/vector-icons'; 
-import { MaterialIcons } from '@expo/vector-icons'; 
-import AddEventForm from './AddEventForm'; 
+import { AntDesign } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import AddEventForm from './AddEventForm';
 
 
-export default function Events(){ 
+export default function Events() {
 
-    const [modalOpen,setModalOpen] = useState(false);    
-    const [events,setEvents] = useState([
-        {host:'Cristina',title:'Lunch',datetime:'today 12 AM',key:'1',place:'Sebesel',maxPers:6,actualPers:3},
-        {host:'Ana',title:'Dinner',datetime:'tomorrow',key:'2',place:'Tg Jiu',maxPers:6,actualPers:6},
-        {host:'Vali',title:'Movie',datetime:'tomorrow',key:'3',place:'Tg Jiu',maxPers:6,actualPers:3},
-        {host:'Mama',title:'Movie',datetime:'tomorrow',key:'4',place:'Tg Jiu',maxPers:6,actualPers:2},
-        {host:'Alina',title:'Movie',datetime:'tomorrow',key:'5',place:'Tg Jiu',maxPers:8,actualPers:6},
-        {host:'Gosa',title:'Movie',datetime:'tomorrow',key:'6',place:'Tg Jiu',maxPers:4,actualPers:0},
-        {host:'Dori',title:'Movie',datetime:'tomorrow',key:'7',place:'Tg Jiu',maxPers:4,actualPers:0},
-        {host:'Mihai',title:'Movie',datetime:'tomorrow',key:'8',place:'Tg Jiu',maxPers:4,actualPers:0},
+    const [modalOpen, setModalOpen] = useState(false);
+    const [events, setEvents] = useState([
+        { host: 'Cristina', title: 'Lunch', datetime: 'today 12 AM', key: '1', place: 'Sebesel', maxPers: 6, actualPers: 3 },
+        { host: 'Ana', title: 'Dinner', datetime: 'tomorrow', key: '2', place: 'Tg Jiu', maxPers: 6, actualPers: 6 },
+        { host: 'Vali', title: 'Movie', datetime: 'tomorrow', key: '3', place: 'Tg Jiu', maxPers: 6, actualPers: 3 },
+        { host: 'Mama', title: 'Movie', datetime: 'tomorrow', key: '4', place: 'Tg Jiu', maxPers: 6, actualPers: 2 },
+        { host: 'Alina', title: 'Movie', datetime: 'tomorrow', key: '5', place: 'Tg Jiu', maxPers: 8, actualPers: 6 },
+        { host: 'Gosa', title: 'Movie', datetime: 'tomorrow', key: '6', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
+        { host: 'Dori', title: 'Movie', datetime: 'tomorrow', key: '7', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
+        { host: 'Mihai', title: 'Movie', datetime: 'tomorrow', key: '8', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
     ]);
 
     const addEvent = (myevent) => {
@@ -34,133 +36,130 @@ export default function Events(){
         });
         setModalOpen(false);
     }
-        return(
-            <View style = {styles.container}>
-                <Entypo name="add-to-list"
-                        size={30}
+    return (
+        <View style={styles.container}>
+            <Entypo name="add-to-list"
+                size={30}
+                color="black"
+                style={styles.addEventButton}
+                onPress={() => setModalOpen(true)}
+            />
+
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <AddEventForm addEvent={addEvent} />
+                    <MaterialIcons name="arrow-back"
+                        size={40}
                         color="black"
-                        style={styles.addEventButton}
-                        onPress={() => setModalOpen(true)}
-                        />
-                
-                <Modal visible={modalOpen} animationType='slide'>
-                    <View style={styles.modalContent}>
-                        <AddEventForm addEvent={addEvent}/>
-                        <MaterialIcons name="arrow-back"
-                                        size={40} 
-                                        color="black"
-                                        style={styles.backButton}
-                                        onPress={() => setModalOpen(false)}
-                                        />
-                    </View>
-                </Modal>
-                <ScrollView>
-                {events.map(item =>  (
+                        style={styles.backButton}
+                        onPress={() => setModalOpen(false)}
+                    />
+                </View>
+            </Modal>
+            <ScrollView>
+                {events.map(item => (
                     <Card key={item.key}>
-                    <Text style={styles.host}>{item.host}</Text>
+                        <Text style={styles.host}>{item.host}</Text>
                         <Image
-                        style={styles.userImage} 
-                        source={require('../assets/default.png')}
-                        /> 
-                       <Text style={styles.title}>
-                        {item.title} {item.place}
-                            </Text>
+                            style={styles.userImage}
+                            source={require('../assets/default.png')}
+                        />
+                        <Text style={styles.title}>
+                            {item.title} {item.place}
+                        </Text>
                         <Text style={styles.datetime}>{item.datetime}</Text>
                         <Text style={styles.available}>Availability: {item.actualPers}/{item.maxPers}</Text>
-                        <TouchableOpacity 
-                            key = {item.key}
+                        <TouchableOpacity
+                            key={item.key}
                             onPress={() => console.log('Successfully join to %s event!', item.host)}
                             style={styles.joinButton}>
                             <AntDesign name="adduser" size={30} color="black" />
                         </TouchableOpacity>
                     </Card>
-                    )
+                )
                 )}
-                </ScrollView>
-            </View>
-        );
-    }
+            </ScrollView>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#fff',
-        paddingTop:50,
-        paddingHorizontal:20
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 50,
+        paddingHorizontal: 20
 
     },
-    header:{
-        marginTop:30, //space between regions
-        padding:10,
-        paddingLeft:90,
+    header: {
+        marginTop: 30, //space between regions
+        padding: 10,
+        paddingLeft: 90,
         backgroundColor: '#D9C6BF'//#66CCCC'
     },
-    title:{
-        fontSize:20,
-        color:'#8C625E',
-        paddingLeft:90,
-        position:'absolute',
+    title: {
+        fontSize: 20,
+        color: '#8C625E',
+        paddingLeft: 90,
+        position: 'absolute',
     },
-    userImage:{
-        flex:1,
-        // position:'absolute',
-        width:70,
-        height:70,
-        borderRadius:30,
-    },
-    host:{
-        fontSize:20,
-        // textAlign: 'center',
-        color:'#8C625E',
-        position:'absolute',
-        // alignContent:'center',
-        left:15,
-        top:-28
-    },
-    datetime:{
-        color:'#8C625E',
-        fontSize:17,
-        position:'absolute',
-        left:90,
-        top:22
-    },
-    available:{
-        color:'#8C625E',
-        fontSize:17,
-        position:'absolute',
-        left:90,
-        top:40
-    },
-    joinButton:{
+    userImage: {
+        flex: 1,
         width: 70,
         height: 70,
-        color:'#8C625E',
+        borderRadius: 30,
+    },
+    host: {
+        fontSize: 20,
+        color: '#8C625E',
+        position: 'absolute',
+        left: 15,
+        top: -28
+    },
+    datetime: {
+        color: '#8C625E',
+        fontSize: 17,
+        position: 'absolute',
+        left: 90,
+        top: 22
+    },
+    available: {
+        color: '#8C625E',
+        fontSize: 17,
+        position: 'absolute',
+        left: 90,
+        top: 40
+    },
+    joinButton: {
+        width: 70,
+        height: 70,
+        color: '#8C625E',
         justifyContent: 'center',
         alignItems: 'center',
         padding: 10,
         borderRadius: 100,
         backgroundColor: '#F2E0D5',
         alignSelf: 'flex-end',
-        position:'absolute',      
-        opacity:1  
+        position: 'absolute',
+        opacity: 1
     },
-    addEventButton:{
-        borderWidth:3,
-        borderColor:"#f2f2f2",
-        padding:10,
-        borderRadius:10,
-        alignSelf:'center'
+    addEventButton: {
+        borderWidth: 3,
+        borderColor: "#f2f2f2",
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center'
     },
-    backButton:{
-        borderWidth:3,
-        borderColor:"#f2f2f2",
-        marginTop:50,
-        marginLeft:10,
-        borderRadius:10,
-        position:'absolute'
+    backButton: {
+        borderWidth: 3,
+        borderColor: "#f2f2f2",
+        marginTop: 50,
+        marginLeft: 10,
+        borderRadius: 10,
+        position: 'absolute'
     },
-    modalContent:{
-        flex:1
+    modalContent: {
+        flex: 1
     }
 
 });
