73562df73c3c0c32904764f09580aee8469451f2
ana.letca <anadoraletca@gmail.com>
2020-12-28 23:09:38 +0200

    add axios call

--- Components/EventsScreen.jsx
+++ Components/EventsScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     StyleSheet,
     Text,
@@ -6,82 +6,141 @@ import {
     ScrollView,
     Image,
     TouchableOpacity,
-    Modal
+    Modal,
+    RefreshControl
 } from 'react-native';
 import Card from "./shared/Card";
 import { AntDesign } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import AddEventForm from './AddEventForm';
+import axios from 'axios';
+
+const wait = (timeout) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+}
 
 
 export default function Events() {
+  const [refreshing, setRefreshing] = useState(false);
+  const [rerun, setRerun] = useState(false);
 
-    const [modalOpen, setModalOpen] = useState(false);
-    const [events, setEvents] = useState([
-        { host: 'Cristina', title: 'Lunch', datetime: 'today 12 AM', key: '1', place: 'Sebesel', maxPers: 6, actualPers: 3 },
-        { host: 'Ana', title: 'Dinner', datetime: 'tomorrow', key: '2', place: 'Tg Jiu', maxPers: 6, actualPers: 6 },
-        { host: 'Vali', title: 'Movie', datetime: 'tomorrow', key: '3', place: 'Tg Jiu', maxPers: 6, actualPers: 3 },
-        { host: 'Mama', title: 'Movie', datetime: 'tomorrow', key: '4', place: 'Tg Jiu', maxPers: 6, actualPers: 2 },
-        { host: 'Alina', title: 'Movie', datetime: 'tomorrow', key: '5', place: 'Tg Jiu', maxPers: 8, actualPers: 6 },
-        { host: 'Gosa', title: 'Movie', datetime: 'tomorrow', key: '6', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
-        { host: 'Dori', title: 'Movie', datetime: 'tomorrow', key: '7', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
-        { host: 'Mihai', title: 'Movie', datetime: 'tomorrow', key: '8', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
-    ]);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
 
-    const addEvent = (myevent) => {
-        myevent.key = events.length + 1;//create key
-        setEvents((currentEvents) => {
-            return [myevent, ...currentEvents]
+    wait(2000).then(() => setRefreshing(false));
+    if(rerun == false)
+      setRerun(true);
+    else
+      setRerun(false);
+  }, []);
+
+  const [eventList, setEventList] = useState([]);
+  useEffect(() => {
+        axios.get('http://192.168.0.175:8080/getEvents').then(res => {
+          setEventList(res.data);
+           console.log(res.data);
         });
-        setModalOpen(false);
-    }
-    return (
-        <View style={styles.container}>
-            <Entypo name="add-to-list"
-                size={30}
-                color="black"
-                style={styles.addEventButton}
-                onPress={() => setModalOpen(true)}
-            />
-
-            <Modal visible={modalOpen} animationType='slide'>
-                <View style={styles.modalContent}>
-                    <AddEventForm addEvent={addEvent} />
-                    <MaterialIcons name="arrow-back"
-                        size={40}
-                        color="black"
-                        style={styles.backButton}
-                        onPress={() => setModalOpen(false)}
-                    />
-                </View>
-            </Modal>
-            <ScrollView>
-                {events.map(item => (
-                    <Card key={item.key}>
-                        <Text style={styles.host}>{item.host}</Text>
-                        <Image
-                            style={styles.userImage}
-                            source={require('../assets/default.png')}
-                        />
-                        <Text style={styles.title}>
-                            {item.title} {item.place}
-                        </Text>
-                        <Text style={styles.datetime}>{item.datetime}</Text>
-                        <Text style={styles.available}>Availability: {item.actualPers}/{item.maxPers}</Text>
-                        <TouchableOpacity
-                            key={item.key}
-                            onPress={() => console.log('Successfully join to %s event!', item.host)}
-                            style={styles.joinButton}>
-                            <AntDesign name="adduser" size={30} color="black" />
-                        </TouchableOpacity>
-                    </Card>
-                )
-                )}
-            </ScrollView>
-        </View>
-    );
+      },[rerun])
+        return (
+            <ScrollView
+            refreshControl={
+                      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+                    }
+            >
+                            {eventList.map(item => (
+                                <Card>
+                                    <Text style={styles.host}> to do </Text>
+                                    <Image
+                                        style={styles.userImage}
+                                        source={require('../assets/default.png')}
+                                    />
+                                    <Text style={styles.title}>
+                                        {item.title} {item.place}
+                                    </Text>
+                                    <Text style={styles.datetime}>{item.datetime}</Text>
+                                    <Text style={styles.available}>Availability: to do/{item.maxPers}</Text>
+                                    <TouchableOpacity
+                                        onPress={() => console.log('Successfully joined event!')}
+                                        style={styles.joinButton}>
+                                        <AntDesign name="adduser" size={30} color="black" />
+                                    </TouchableOpacity>
+                                </Card>
+                            )
+                            )}
+                        </ScrollView>
+          );
 }
+
+// export default function Events() {
+//
+//     const [modalOpen, setModalOpen] = useState(false);
+//     const [events, setEvents] = useState([
+//         { host: 'Cristina', title: 'Lunch', datetime: 'today 12 AM', key: '1', place: 'Sebesel', maxPers: 6, actualPers: 3 },
+//         { host: 'Ana', title: 'Dinner', datetime: 'tomorrow', key: '2', place: 'Tg Jiu', maxPers: 6, actualPers: 6 },
+//         { host: 'Vali', title: 'Movie', datetime: 'tomorrow', key: '3', place: 'Tg Jiu', maxPers: 6, actualPers: 3 },
+//         { host: 'Mama', title: 'Movie', datetime: 'tomorrow', key: '4', place: 'Tg Jiu', maxPers: 6, actualPers: 2 },
+//         { host: 'Alina', title: 'Movie', datetime: 'tomorrow', key: '5', place: 'Tg Jiu', maxPers: 8, actualPers: 6 },
+//         { host: 'Gosa', title: 'Movie', datetime: 'tomorrow', key: '6', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
+//         { host: 'Dori', title: 'Movie', datetime: 'tomorrow', key: '7', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
+//         { host: 'Mihai', title: 'Movie', datetime: 'tomorrow', key: '8', place: 'Tg Jiu', maxPers: 4, actualPers: 0 },
+//     ]);
+//
+//     const addEvent = (myevent) => {
+//         myevent.key = events.length + 1;//create key
+//         setEvents((currentEvents) => {
+//             return [myevent, ...currentEvents]
+//         });
+//         setModalOpen(false);
+//     }
+//     return (
+//         <View style={styles.container}>
+//             <Entypo name="add-to-list"
+//                 size={30}
+//                 color="black"
+//                 style={styles.addEventButton}
+//                 onPress={() => setModalOpen(true)}
+//             />
+//
+//             <Modal visible={modalOpen} animationType='slide'>
+//                 <View style={styles.modalContent}>
+//                     <AddEventForm addEvent={addEvent} />
+//                     <MaterialIcons name="arrow-back"
+//                         size={40}
+//                         color="black"
+//                         style={styles.backButton}
+//                         onPress={() => setModalOpen(false)}
+//                     />
+//                 </View>
+//             </Modal>
+//             <ScrollView>
+//                 {events.map(item => (
+//                     <Card key={item.key}>
+//                         <Text style={styles.host}>{item.host}</Text>
+//                         <Image
+//                             style={styles.userImage}
+//                             source={require('../assets/default.png')}
+//                         />
+//                         <Text style={styles.title}>
+//                             {item.title} {item.place}
+//                         </Text>
+//                         <Text style={styles.datetime}>{item.datetime}</Text>
+//                         <Text style={styles.available}>Availability: {item.actualPers}/{item.maxPers}</Text>
+//                         <TouchableOpacity
+//                             key={item.key}
+//                             onPress={() => console.log('Successfully join to %s event!', item.host)}
+//                             style={styles.joinButton}>
+//                             <AntDesign name="adduser" size={30} color="black" />
+//                         </TouchableOpacity>
+//                     </Card>
+//                 )
+//                 )}
+//             </ScrollView>
+//         </View>
+//     );
+// }
 
 const styles = StyleSheet.create({
     container: {
@@ -89,7 +148,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         paddingTop: 50,
         paddingHorizontal: 20
-
     },
     header: {
         marginTop: 30, //space between regions
