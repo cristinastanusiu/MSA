611f8755ef148731c248c47cbc3e3f573b14a345
stanusiucristina <cristina.stanusiu@coreai.ai>
2021-01-03 00:57:32 +0200

    add user profile screen

--- Components/ProfileScreen.jsx
+++ Components/ProfileScreen.jsx
@@ -1,83 +1,134 @@
-import React from 'react';
-import {View, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View,
+    SafeAreaView,
+    StyleSheet,
+    Image,
+    Dimensions,
+    ScrollView,
+    RefreshControl,
+    TouchableOpacity} from 'react-native';
 import {
-    Avatar,
-    Title,
-    Caption,
     Text,
-    TouchableRipple,
 } from 'react-native-paper';
+import {ActionSheet,Root}  from 'native-base';
+import ImagePicker from 'react-native-image-crop-picker';
 
-const ProfileScreen = () => {
+const wait = (timeout) => {
+    return new Promise(resolve => {
+        setTimeout(resolve, timeout);
+    });
+}
+const width = Dimensions.get('window').width;
+
+export default function ProfileScreen() {
+    const [refreshing, setRefreshing] = useState(false);
+    const [rerun, setRerun] = useState(false);
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+
+        wait(2000).then(() => setRefreshing(false));
+        if(rerun == false)
+            setRerun(true);
+        else
+            setRerun(false);
+    }, []);
+    //
+    // const [eventList, setEventList] = useState([]);
+    // useEffect(() => {
+    //     axios.get('http://192.168.0.175:8080/getEvents').then(res => {
+    //         setEventList(res.data);
+    //         console.log(res.data);
+    //     });
+    // },[rerun])
+
+    const takePhotoFromCamera = () =>{
+        ImagePicker.openCamera(
+            {
+                width:300,
+                height:400,
+                cropping: true
+            }).then(image => {
+            console.log(image);
+        });
+    }
+
+    const choosePhotoFromLibrary = () =>{
+        ImagePicker.openPicker(
+            {
+                width:300,
+                height:400,
+                cropping: true
+            }).then(image => {
+                console.log(image);
+        });
+    };
+    const ClickAddImage = () =>{
+        const BUTTONS = ['Take Photo','Choose Photo Library','Cancel'];
+        ActionSheet.show(
+            {options:BUTTONS,cancelButtonIndex:2,title:'Select a Photo'},
+            (buttonIndex) => {
+                switch (buttonIndex){
+                    case 0:
+                        takePhotoFromCamera();
+                    case 1:
+                        choosePhotoFromLibrary();
+                    default:
+                        break;
+
+                }
+            }
+        )
+    };
+
+    const renderImage = ({item,index}) => {
+        return (
+            <View>
+                <Image source = {item.url} style = {styles.itemImage}/>
+            </View>
+        )
+    };
 
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.userInfoSection}>
-                <View>
-                    <Avatar.Image
-                        source={{
-                            uri: 'https://api.adorable.io/avatars/80/abott@adorable.png',
-                        }}
-                        size={80}
-                    />
-                </View>
-                <View>
+    <Root>
+        <ScrollView
+            refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }>
 
-                </View>
+            <View style={styles.content} >
+                <Text>User profile </Text>
+                <TouchableOpacity onPress={ClickAddImage} style = {styles.btnAddImage}>
+                    <Text style = {styles.txtBtn}>Add Profile Image</Text>
+                </TouchableOpacity>
             </View>
-        </SafeAreaView>
+        </ScrollView>
+    </Root>
     );
-};
+}
 
-export default ProfileScreen;
 
 const styles = StyleSheet.create({
-    container: {
+    content: {
         flex: 1,
+        alignItems:'center',
+        marginTop: 60,
     },
-    userInfoSection: {
-        paddingHorizontal: 30,
-        marginBottom: 25,
+    btnAddImage: {
+        backgroundColor: "#0080ff",
+        height:50,
+        width: width - 60,
+        alignItems:'center',
+        justifyContent:'center'
+        },
+    txtBtn:{
+        color:'#ffffff',
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-    caption: {
-        fontSize: 14,
-        lineHeight: 14,
-        fontWeight: '500',
-    },
-    row: {
-        flexDirection: 'row',
-        marginBottom: 10,
-    },
-    infoBoxWrapper: {
-        borderBottomColor: '#dddddd',
-        borderBottomWidth: 1,
-        borderTopColor: '#dddddd',
-        borderTopWidth: 1,
-        flexDirection: 'row',
-        height: 100,
-    },
-    infoBox: {
-        width: '50%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    menuWrapper: {
-        marginTop: 10,
-    },
-    menuItem: {
-        flexDirection: 'row',
-        paddingVertical: 15,
-        paddingHorizontal: 30,
-    },
-    menuItemText: {
-        color: '#777777',
-        marginLeft: 20,
-        fontWeight: '600',
-        fontSize: 16,
-        lineHeight: 26,
-    },
+    itemImage:{
+        backgroundColor: '#2F455C',
+        height: 150,
+        width:width  - 60,
+        borderRadius: 15,
+        resizeMode:'contain'
+    }
 });
