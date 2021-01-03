6ec06bc6639645bf81c6e61e1a0d4db38fdcee2b
stanusiucristina <cristina.stanusiu@coreai.ai>
2021-01-03 22:57:10 +0200

    add user image

--- Components/ProfileScreen.jsx
+++ Components/ProfileScreen.jsx
@@ -6,12 +6,16 @@ import {View,
     Dimensions,
     ScrollView,
     RefreshControl,
+    CameraRoll,
     TouchableOpacity} from 'react-native';
 import {
     Text,
 } from 'react-native-paper';
 import {ActionSheet,Root}  from 'native-base';
-import ImagePicker from 'react-native-image-crop-picker';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+
 
 const wait = (timeout) => {
     return new Promise(resolve => {
@@ -24,6 +28,21 @@ export default function ProfileScreen() {
     const [refreshing, setRefreshing] = useState(false);
     const [rerun, setRerun] = useState(false);
 
+    const [image, setImage] = useState(null);
+    const [camera, setCamera] = useState(false);
+
+
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        })();
+    }, []);
+
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
 
@@ -33,36 +52,40 @@ export default function ProfileScreen() {
         else
             setRerun(false);
     }, []);
-    //
-    // const [eventList, setEventList] = useState([]);
-    // useEffect(() => {
-    //     axios.get('http://192.168.0.175:8080/getEvents').then(res => {
-    //         setEventList(res.data);
-    //         console.log(res.data);
-    //     });
-    // },[rerun])
 
-    const takePhotoFromCamera = () =>{
-        ImagePicker.openCamera(
-            {
-                width:300,
-                height:400,
-                cropping: true
-            }).then(image => {
-            console.log(image);
-        });
-    }
+    const takePhotoFromCamera = async () => {
+        const response1 = await Permissions.askAsync(Permissions.CAMERA);
+        const response2 = await Permissions.askAsync(Permissions.CAMERA_ROLL);
 
-    const choosePhotoFromLibrary = () =>{
-        ImagePicker.openPicker(
-            {
-                width:300,
-                height:400,
-                cropping: true
-            }).then(image => {
-                console.log(image);
-        });
+        if (response1.granted & response2.granted) {
+            let result = await ImagePicker.launchCameraAsync({
+                allowsEditing: false,
+                aspect: [4, 3],
+                quality: 1,
+            });
+            console.log(result);
+            if (!result.cancelled) {
+                setImage(result.uri);
+                await MediaLibrary.saveToLibraryAsync(result.uri);
+            }
+            return;
+        }
+        console.log('Camera or Camera Roll perimission denied!');
     };
+
+    const choosePhotoFromLibrary = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+        console.log(result);
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
     const ClickAddImage = () =>{
         const BUTTONS = ['Take Photo','Choose Photo Library','Cancel'];
         ActionSheet.show(
@@ -70,9 +93,13 @@ export default function ProfileScreen() {
             (buttonIndex) => {
                 switch (buttonIndex){
                     case 0:
+                        console.log("Clicked 0 on button Index"+ buttonIndex);
                         takePhotoFromCamera();
+                        break;
                     case 1:
+                        console.log("Clicked 1  on button Index"+ buttonIndex);
                         choosePhotoFromLibrary();
+                        break;
                     default:
                         break;
 
@@ -81,29 +108,21 @@ export default function ProfileScreen() {
         )
     };
 
-    const renderImage = ({item,index}) => {
-        return (
-            <View>
-                <Image source = {item.url} style = {styles.itemImage}/>
-            </View>
-        )
-    };
-
     return (
-    <Root>
-        <ScrollView
-            refreshControl={
-                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }>
-
-            <View style={styles.content} >
-                <Text>User profile </Text>
-                <TouchableOpacity onPress={ClickAddImage} style = {styles.btnAddImage}>
-                    <Text style = {styles.txtBtn}>Add Profile Image</Text>
-                </TouchableOpacity>
-            </View>
-        </ScrollView>
-    </Root>
+        <Root>
+                <ScrollView
+                    refreshControl={
+                        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+                    }>
+                    <View style={styles.content} >
+                        <Text>User profile </Text>
+                        <TouchableOpacity onPress={ClickAddImage} style = {styles.btnAddImage}>
+                            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200,marginTop: 400 }} />}
+                            <Text style = {styles.txtBtn}>Add Profile Image</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+            </Root>
     );
 }
 
