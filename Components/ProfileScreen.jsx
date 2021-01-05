cbbe97f1c8e098c44987154064461dd1af760c0f
stanusiucristina <cristina.stanusiu@coreai.ai>
2021-01-06 01:29:02 +0200

    add events history

--- Components/ProfileScreen.jsx
+++ Components/ProfileScreen.jsx
@@ -1,21 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import {View,
+    Text,
     StyleSheet,
     Image,
     ScrollView,
     RefreshControl,
     Alert,
     TouchableOpacity} from 'react-native';
-import {
-    Text,
-} from 'react-native-paper';
 import {ActionSheet,Root}  from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import * as MediaLibrary from 'expo-media-library';
 import {Platform} from "react-native-web";
 import * as firebase from 'firebase';
+import Card from "./shared/Card";
+import {AntDesign} from "@expo/vector-icons";
+import axios from "axios";
 
+const imgName = "Cristina"
 
 const wait = (timeout) => {
     return new Promise(resolve => {
@@ -23,25 +25,23 @@ const wait = (timeout) => {
     });
 }
 
-
-const uploadImage = async (uri) => {
+const uploadImage = async (uri,imgName) => {
     const response = await fetch(uri);
     const blob = await response.blob();
-    console.log("FIREBASE IS  "  + firebase.name)
-
-    console.log("uri is "  + uri)
-    console.log("response is "  + response)
-    console.log("Blob is "  + blob)
-
-    let ref = firebase.storage().ref().child('test');
+    let ref = firebase.storage().ref().child('images/' + imgName);
     return ref.put(blob);
+}
 
+const retrieveImage = async (imgName) => {
+    const ref = firebase.storage().ref("images/" + imgName);
+    return ref.getDownloadURL();
 }
 
 export default function ProfileScreen() {
     const [refreshing, setRefreshing] = useState(false);
     const [rerun, setRerun] = useState(false);
     const [image, setImage] = useState(null);
+    const [eventsHistory, setEventsHisyory] = useState(null);
 
     useEffect(() => {
         (async () => {
@@ -56,13 +56,22 @@ export default function ProfileScreen() {
 
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
-
+        getUserEvents();
         wait(2000).then(() => setRefreshing(false));
         if(rerun == false)
             setRerun(true);
         else
             setRerun(false);
     }, []);
+
+    const getUserEvents = () => {
+        axios.get('http://ec2-35-179-96-157.eu-west-2.compute.amazonaws.com:8080/getEvents').then(res => {
+            var key_cnt = 0;
+            res.data.map(e => {e.key = key_cnt; key_cnt = key_cnt + 1;})
+            setEventsHisyory(res.data);
+            console.log(res.data);
+        });
+    }
 
     const takePhotoFromCamera = async () => {
         const response1 = await Permissions.askAsync(Permissions.CAMERA);
@@ -72,16 +81,21 @@ export default function ProfileScreen() {
             let result = await ImagePicker.launchCameraAsync({
                 allowsEditing: false,
                 aspect: [4, 3],
-                quality: 1,
+                maxWidth: 150,
+                maxHeight: 150,
+                quality: 3,
             });
             console.log(result);
             if (!result.cancelled) {
                 setImage(result.uri);
                 await MediaLibrary.saveToLibraryAsync(result.uri);
             }
-            await uploadImage(result.uri).then(()=>
+            await uploadImage(result.uri,imgName)
+                .then(()=>
             {
-                Alert.alert("successs");
+                Alert.alert(
+                    "Done!",
+                    "Your profile picture has been updated successfully");
             });
             return;
         }
@@ -92,15 +106,19 @@ export default function ProfileScreen() {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
+            maxWidth: 150,
+            maxHeight: 150,
             aspect: [4, 3],
-            quality: 1,
+            quality: 3,
         });
         console.log(result);
         if (!result.cancelled) {
             setImage(result.uri);
-            await uploadImage(result.uri).then(()=>
+            await uploadImage(result.uri,imgName).then(()=>
             {
-                Alert.alert("successs");
+                Alert.alert(
+                    "Done!",
+                    "Your profile picture has been updated successfully");
             });
         }
 
@@ -128,9 +146,10 @@ export default function ProfileScreen() {
         )
     };
 
+
     return (
         <Root>
-                <ScrollView
+            <ScrollView
                     refreshControl={
                         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
                     }>
@@ -141,10 +160,37 @@ export default function ProfileScreen() {
                             <Text style = {styles.txtBtn}>Edit Profile Picture</Text>
                         </TouchableOpacity>
                     </View>
+
+                    {eventsHistory.map(item => (
+                        <Card>
+                            <Text style={styles.host}> to do </Text>
+                            <Text style={styles.title}>
+                                {item.title} {item.place}
+                            </Text>
+                            <Text style={styles.datetime}>{item.datetime}</Text>
+                            <Text style={styles.available}>Availability: to do/{item.maxPers}</Text>
+                        </Card>)
+                    )}
+                <TouchableOpacity onPress={() => console.log('Successfully LOGGED OUT!')} style={styles.logoutBtn}>
+                    <AntDesign name="logout" size={30} color="black" />
+                </TouchableOpacity>
                 </ScrollView>
             </Root>
-    );
+
+);
 }
+
+//get image from firebase
+// const [retrImage, setRetrImage] = useState(null);
+//
+// retrieveImage(imgName)
+//     .then(url => {
+//         console.log("URL is " + url);
+//         setRetrImage(url);
+//     })
+//     .catch((err) => {
+//         console.log("File" +imgName+ " not found in firebase " +err );
+//     });
 
 
 const styles = StyleSheet.create({
@@ -171,5 +217,46 @@ const styles = StyleSheet.create({
         borderRadius: 60,
         marginTop: 0,
         resizeMode:'contain'
+    },
+    title: {
+        fontSize: 20,
+        color: '#8C625E',
+        paddingLeft: 90,
+        position: 'absolute',
+    },
+    host: {
+        fontSize: 20,
+        color: '#8C625E',
+        position: 'absolute',
+        left: 15,
+        top: -28
+    },
+    datetime: {
+        color: '#8C625E',
+        fontSize: 17,
+        position: 'absolute',
+        left: 90,
+        top: 22
+    },
+    available: {
+        color: '#8C625E',
+        fontSize: 17,
+        position: 'absolute',
+        left: 90,
+        top: 40
+    },
+    logoutBtn: {
+        width: 70,
+        height: 70,
+        color: '#8C625E',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        borderRadius: 100,
+        backgroundColor: '#F2E0D5',
+        alignSelf: 'flex-end',
+        position: 'absolute',
+        opacity: 1
+
     }
 });
--- Components/constants/ApiKeys.jsx
+++ Components/constants/ApiKeys.jsx
@@ -1,12 +1,12 @@
 
 export default {
     firebaseConfig:{
-        apiKey: "",
-        authDomain: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: "",
-        measurementId: ""
+        apiKey: "AIzaSyB_QyZV5R4lEwlbcUjB_hC3s9f9_gDseKI",
+        authDomain: "itsybitsy-56c47.firebaseapp.com",
+        projectId: "itsybitsy-56c47",
+        storageBucket: "itsybitsy-56c47.appspot.com",
+        messagingSenderId: "65444893623",
+        appId: "1:65444893623:web:038d8964ec56bbad250e32",
+        measurementId: "G-1H0X5MGBV4"
     }
 };
